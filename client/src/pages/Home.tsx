f32604c16ab3befa1d421728ba1110deb3ebb3a7
Ash <aishasiddiqa151@gmail.com>
2024-11-17 15:50:14 -0700

    Create Homepage

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <header className="home-header">
+        <h1>Welcome to PetPal</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
