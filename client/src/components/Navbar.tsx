4c1fa7db403c4d5007faa11c22f35302ed7b4df1
Ash <aishasiddiqa151@gmail.com>
2024-11-17 20:35:19 -0700

    Create assets folder and import logo from images

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
+import PetPalLogo from '../assets/images/PetPalLogo.png';
 
 const Navbar = () => {
   const isLoggedIn = Auth.loggedIn();
 
   return (
     <nav className="navbar">
+      <img className='logo' src={PetPalLogo} alt="PetPal Logo" />
     <ul className='nav-links'>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
