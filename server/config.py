03c08410723cf3ad40811ea8fa3521cb5d47eb90
Your Name <kboothe617@gmail.com>
2024-11-19 20:13:23 -0700

    config.py email and app configurations

--- server/config.py
+++ server/config.py
@@ -5,3 +5,15 @@ DATABASE = {
     'user': 'your_username',
     'database': 'your_database'
 }
+
+# email configuration
+EMAIL = {
+    'username': 'your_email',
+    'password': 'your_password'
+}
+
+# application configuration
+APP_SETTINGS = {
+    'debug': True,
+    'secret_key': 'your_secret_key'
+}
