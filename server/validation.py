95567d8a1e5386283fde4aaf399fa38218ddd4cf
Your Name <kboothe617@gmail.com>
2024-11-19 19:57:06 -0700

    validation.py password validation function

--- server/validation.py
+++ server/validation.py
@@ -4,3 +4,15 @@ import re
 def validate_email(email):
     pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
     return re.match(pattern, email) is not None
+
+# password validation
+def validate_password(password):
+    if len(password) < 8:
+        return False
+    if not re.search(r'[A-Z]', password):
+        return False
+    if not re.search(r'[a-z]', password):
+        return False
+    if not re.search(r'[0-9]', password):
+        return False
+    return True
