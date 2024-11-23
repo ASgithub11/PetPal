5ff17e91adda953bc0bb61c23ad2ca33ce9cec41
Your Name <kboothe617@gmail.com>
2024-11-23 16:20:23 -0700

    root to authentication file for test

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,6 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
 from datetime import datetime, timedelta
-from authentication import generate_token, verify_token
+from server.authentication import generate_token, verify_token
 import jwt
 
 class TestAuthentication(unittest.TestCase):
